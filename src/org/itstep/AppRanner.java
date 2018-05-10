cd2c96b90344d9d6b951a6cf65a401fa679b1ecf
student <student@K04-08.lan.itstep.kiev.ua>
2018-05-10 19:46:47 +0300

    my first commit

---
+++ src/org/itstep/AppRanner.java
@@ -0,0 +1,13 @@
+package org.itstep;
+
+public class AppRanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Name firstName = new Name();
+String myName = firstName.getName("Misha");
+System.out.println(myName);
+	}
+
+}
